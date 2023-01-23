e425caa22449617048ac198c210ffa8b498197d9
Christian Ingles <christianarguetaaa@gmail.com>
2023-01-22 20:54:26 -0600

    Add the values ​​of the operation to the screen

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+const equal = document.querySelector("#equal");
+const showResult = document.querySelector("#showResult");
+const addCharacters = document.querySelectorAll(".add");
+
+let operation = "";
+let continueOperation = true;
+
+addCharacters.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (
+      operation[operation.length - 1] === "+" ||
+      operation[operation.length - 1] === "-" ||
+      operation[operation.length - 1] === "*" ||
+      operation[operation.length - 1] === "/"
+    ) {
+      continueOperation = false;
+    } else {
+      continueOperation = true;
+    }
+
+    if (
+      continueOperation === false &&
+      (element.textContent == "+" ||
+        element.textContent == "-" ||
+        element.textContent == "*" ||
+        element.textContent == "/")
+    ) {
+      operation = operation.substring(0, operation.length - 1);
+    } else {
+      operation += element.textContent;
+      showResult.textContent = operation;
+    }
+  });
+});
